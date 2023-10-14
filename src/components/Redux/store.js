ee09b1ca4d47a7e57960e167c0ef930a64f41f3e
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-10-14 16:55:51 +0300

    first settings

--- src/components/Redux/store.js
+++ src/components/Redux/store.js
@@ -1,40 +1,41 @@
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import {
+//   persistReducer,
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import contactsAndFilterReducer from './contactsAndFilterSlice';
 import nameAndNumberReducer from './nameAndNumberSlice';
 
-const persistConfig = {
-  key: 'items',
-  storage,
-  blacklist: ['nameAndNumber'],
-};
+// const persistConfig = {
+//   key: 'items',
+//   storage,
+//   blacklist: ['nameAndNumber'],
+// };
 
 const rootReducer = combineReducers({
   contactsAndFilter: contactsAndFilterReducer,
   nameAndNumber: nameAndNumberReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  // reducer: persistedReducer,
+  reducer: rootReducer,
+  // middleware: getDefaultMiddleware =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './components/Redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor, store } from './components/Redux/store';
+import { store } from './components/Redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      {/* <PersistGate persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
